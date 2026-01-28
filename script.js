30d8d66b15878578a4f2e4b569e2d840a0e75a62
lingaraj2002 <lingaraj01052002@gmail.com>
2026-01-29 00:50:22 +0530

    commit-nav-and-typo-fix-29012026-1250

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function toggleMenu() {
-  const nav = document.getElementById("navMenu");
-  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+const nav = document.getElementById("navMenu");
+const closeLinks = document.querySelectorAll(".close-link");
+
+function toggleMenu(value) {
+  if (value === "0") {
+    nav.style.display = "flex";
+    setTimeout(() => (nav.style.right = value), 200);
+  } else {
+    nav.style.right = value;
+    setTimeout(() => (nav.style.display = "none"), 200);
+  }
 }
+
+closeLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    toggleMenu("-100%");
+  });
+});
